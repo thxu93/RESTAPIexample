e432621aa8a77f835a8590f5457c331ac678e30a
Thomas Xu <thomasxu23@gmail.com>
2016-02-20 17:17:47 -0800

    test commit

--- RestAPI/ViewController.swift
+++ RestAPI/ViewController.swift
@@ -10,6 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myDatePicker: UIDatePicker!
+    @IBOutlet weak var selectedDate: UILabel!
+
+    
+    @IBOutlet weak var myDeparture: UITextField!
+    @IBOutlet weak var myArrival: UITextField!
+    
+    
+    @IBAction func datePickerAction(sender: AnyObject) {
+        
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        let strDate = dateFormatter.stringFromDate(myDatePicker.date)
+        self.selectedDate.text = strDate
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,7 +34,7 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
+    }   
 
 
 }
