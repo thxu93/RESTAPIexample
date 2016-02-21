7ed63a40aad9cb1460e3e230e2b0cacce114bc0d
Thomas Xu <thomasxu23@gmail.com>
2016-02-20 19:45:20 -0800

    url fixed

--- RestAPI/ViewController.swift
+++ RestAPI/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var myDatePicker: UIDatePicker!
     @IBOutlet weak var selectedDate: UILabel!
 
@@ -21,11 +21,43 @@ class ViewController: UIViewController {
     @IBAction func datePickerAction(sender: AnyObject) {
         
         let dateFormatter = NSDateFormatter()
-        dateFormatter.dateFormat = "yyyy-MM-dd"
+            dateFormatter.dateFormat = "yyyy-MM-dd"
         let strDate = dateFormatter.stringFromDate(myDatePicker.date)
+        print(strDate)
+        
+        
         self.selectedDate.text = strDate
         
     }
+    
+
+    
+    @IBAction func makeTheCall(sender: AnyObject) {
+        
+        let url : String = "http://terminal2.expedia.com/x/mflights/search?departureAirport=" + myDeparture.text! + "&arrivalAirport=" + myArrival.text! + "&departureDate=" + selectedDate.text! + "&childTravelerAge=2&apikey=wc1LMMNRaHdJhHzpLTMdOlNbRSTRsPTU"
+        print(url)
+        
+        
+        
+//        var request : NSMutableURLRequest = NSMutableURLRequest()
+//        request.URL = NSURL(string: url)
+//        request.HTTPMethod = "GET"
+//        
+//        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue(), completionHandler:{ (response:NSURLResponse!, data: NSData!, error: NSError!) -> Void in
+//            var error: AutoreleasingUnsafeMutablePointer<NSError?> = nil
+//            let jsonResult: NSDictionary! = NSJSONSerialization.JSONObjectWithData(data, options:NSJSONReadingOptions.MutableContainers, error: error) as? NSDictionary
+//            
+//            if (jsonResult != nil) {
+//                // process jsonResult
+//            } else {
+//                // couldn't load JSON, look at error
+//            }
+//            
+//            
+//        })
+    
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -35,7 +67,6 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
     
 }
 
